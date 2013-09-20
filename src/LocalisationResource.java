c4e271bd6239c854430c8fca5273460fd3ec15e3
Renaud Bigras <rjlb.lt@gmail.com>
2013-09-20 11:58:08 -0400

    Implémentation du GUI (premier jet) et de la connection serveur.
    Mise à jour de ShapesContainer (Canvas au lieu de JComponent)
    
    Implémentation de LocalisationResource pour les differentes langues.
    
    Test de Github

--- src/LocalisationResource.java
+++ src/LocalisationResource.java
@@ -1,17 +1,52 @@
+/******************************************************
+Cours:  LOG121
+Projet: Laboratoire #1
+Nom du fichier: LocalisationResource.java
+Date créé: 2013-05-03
+*******************************************************
+* Ce code est basé d'un exemple sur Internet, de David Geary, à la page suivante:
+* http://www.javaworld.com/javaworld/jw-05-2003/jw-0530-designpatterns-p2.html
+Historique des modifications
+*******************************************************
+2013-05-03 Version initiale
+*******************************************************/  
+
+import java.util.Locale;
+import java.util.ResourceBundle;	
+
 /**
- * Ecapsule les différentes méthodes permettant de manipuler les ressources
- * relatives à la région ou la langue de l'utilisateur.
- * 
- * @auteur Hugo Lapointe Di Giacomo
- * @date 19 septembre 2013
+ * Utilitaire pour obtenir un mot dans la langue de l'application
  */
 public class LocalisationResource {
-	/**
-	 * Retourne une chaîne de caractères dans la langue de l'utilsateur.
-	 * 
-	 * @param key
-	 */
-	public void getResource(String key) {
-		// TODO Implémenter la méthode générée.
-	}
+	
+	static private final String PREFS_BUNDLE_BASENAME = "prefs";
+	static private final String BUNDLE_BASENAME = "app", PREFERRED_LOCALE_KEY = "locale";
+	private static ResourceBundle preferences, resources;
+	static private Locale locale;
+	
+	static {
+	      try {
+	         preferences = ResourceBundle.getBundle(PREFS_BUNDLE_BASENAME);
+	         locale = new Locale(preferences.getString(PREFERRED_LOCALE_KEY));
+	      }
+	      catch(java.util.MissingResourceException ex) {
+	         System.err.println("ERROR: cannot find preferences properties file " + 
+	                            BUNDLE_BASENAME);
+	      }
+	      try {
+	         resources = ResourceBundle.getBundle(BUNDLE_BASENAME, locale);
+	      }
+	      catch(java.util.MissingResourceException ex) {
+	         System.err.println("ERROR: cannot find properties file for " + BUNDLE_BASENAME);
+	      }
+	   };
+	   
+	   /**
+	    * Retourne un mot (dans la langue de l'application) lié à  un champs
+	    * @param key champs
+	    * @return
+	    */
+	   public static String getResource(String key) {
+		      return (resources == null) ? null : resources.getString(key);
+	   }
 }
--- src/ShapesContainer.java
+++ src/ShapesContainer.java
@@ -1,6 +1,5 @@
+import java.awt.Canvas;
 import java.awt.Graphics;
-
-import javax.swing.JComponent;
 
 /**
  * Représente le composant qui contiendra les formes graphiques envoyés par le
@@ -12,7 +11,7 @@ import javax.swing.JComponent;
  * @auteur Hugo Lapointe Di Giacomo
  * @date 19 septembre 2013
  */
-public class ShapesContainer extends JComponent {
+public class ShapesContainer extends Canvas {
 	private final int SHAPES_COUNT = 10;
 
 	private Shape[] _shapes;
